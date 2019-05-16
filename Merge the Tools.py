372b898ee803552895db38b816e00e14489da4c0
jobairBalam <49426510+jobairBalam@users.noreply.github.com>
2019-05-16 12:58:08 +0600

    Update Merge the Tools.py

--- Merge the Tools.py
+++ Merge the Tools.py
@@ -18,3 +18,19 @@ def main():
 
 
 main()
+
+'''Hacker rank version'''
+
+from collections import OrderedDict
+
+
+def merge_the_tools(string, k):
+    for i in range(0, len(string), k):
+        print(''.join(OrderedDict.fromkeys(string[i:i+k])))
+
+
+
+if __name__=='__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
+
